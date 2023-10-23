1a466c486696175a836635a113289942bee3ed18
jarmoza <jarmoza@gmail.com>
2023-10-23 10:01:03 -0400

    QA Workflow base class

--- qa_utilities.py
+++ qa_utilities.py
@@ -1,6 +1,6 @@
 # Author: Jonathan Armoza
 # Created: October 13, 2023
-# Purpose: Utility functions for Print & Probability QA scripts
+# Purpose: Classes and functions for Print & Probability QA scripts
 
 # Imports
 
@@ -8,11 +8,27 @@
 import os
 
 
+# Classes
+
+class QA_Workflow:
+
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
+
+    def collate_results(self):
+        pass
+
+    def clear_results(self):
+        pass
+
 # Functions
 
 def directory_has_files_of_type(p_book_directory, p_file_tag):
 
-    items = get_items_in_dir(p_book_directory, return_types=["files"])
+    items = get_items_in_dir(format_path(p_book_directory), return_types=["files"])
     found_tif = False
     for item in items:
         if item.lower().endswith(p_file_tag):
@@ -26,15 +42,16 @@ def format_path(original_path):
 def get_items_in_dir(path, return_types=[]):
     '''Get all items in given path of type "directories" and/or "files"'''
 
-    directory_contents = os.listdir(path)
+    formatted_path = format_path(path)
+    directory_contents = os.listdir(formatted_path)
     returned_contents = []
 
     for dI in directory_contents:
         if "directories" in return_types:
-            if os.path.isdir(os.path.join(path, dI)):
+            if os.path.isdir(os.path.join(formatted_path, dI)):
                 returned_contents.append(dI)
         if "files" in return_types:
-            if not os.path.isdir(os.path.join(path, dI)):
+            if not os.path.isdir(os.path.join(formatted_path, dI)):
                 returned_contents.append(dI)
 
     return returned_contents
