a6a8fdcec37706f533db4e17d2b015e695a90e76
jarmoza <jarmoza@gmail.com>
2023-10-24 15:48:33 -0400

    Changing top level output name for slurm logs

--- run_qa.sh
+++ run_qa.sh
@@ -3,6 +3,6 @@
 #SBATCH --mem=1g
 #SBATCH -t 12:00:00
 #SBATCH -p RM-shared
-#SBATCH -o ./logs/qa_all_slurm-%j.out
+#SBATCH -o ./logs/qa_autocrop_slurm-%j.out
 
 python3 qa.py autocrop --config_file qa.yaml
