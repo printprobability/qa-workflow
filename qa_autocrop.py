363318786aedb846ffc058e8553d5e23366048bc
jarmoza <jarmoza@gmail.com>
2023-10-26 22:29:51 -0400

    Adding OO version of autocrop

--- qa_autocrop.py
+++ qa_autocrop.py
@@ -6,7 +6,9 @@
 
 # Built-ins
 import csv
+import glob
 import os
+import shutil
 import subprocess
 from datetime import datetime
 from pathlib import Path
@@ -17,6 +19,7 @@ from PIL import Image
 
 # Custom
 from prepare_alignment_input_csv import *
+from qa_constants import *
 from qa_utilities import *
 
 
@@ -26,6 +29,206 @@ from qa_utilities import *
 AUTOCROP_SCRIPT_LOCATION = "..{0}auto_crop.py".format(os.sep)
 CROPTYPE_THRESHOLD_BY_INSIDE = "threshold_by_inside"
 CROPTYPE_NON_THRESHOLD_BY_INSIDE = "non_threshold_by_inside"
+
+# sbatch parameters
+
+# Classes
+
+class QA_Autocrop(QA_Module):
+
+    def __init__(self, p_config):
+
+        self.config = p_config
+        self.slurm_job_results = []
+
+    def archive_logs(self):
+
+        # 0. Make an 'archive' folder in the output directory if it does not exist
+        if not os.path.exists(self.config[OUTPUT_DIRECTORY] + ARCHIVE_DIRECTORY):
+            os.makedirs(self.config[OUTPUT_DIRECTORY] + ARCHIVE_DIRECTORY)
+
+        # 1. Move each item (that's not the archive folder or master log for this run) into the archive folder
+        for item in get_items_in_dir(self.config[OUTPUT_DIRECTORY], ["directories", "files"]):
+            if ARCHIVE_DIRECTORY != item and MASTER_LOG_FILENAME_PREFIX not in item:
+                os.rename(self.config[OUTPUT_DIRECTORY] + item,
+                    "{0}archive{1}{2}".format(self.config[OUTPUT_DIRECTORY], os.sep, item))
+
+    def clear_logs(self):
+
+        # Clear all log files in the log folder
+        log_filepaths = glob.glob(self.config[OUTPUT_DIRECTORY] + "*.out")
+        for filepath in log_filepaths:
+            os.unlink(filepath)
+
+    def clear_results(self):
+
+        if RUN_TYPE_MULTI == self.config[RUN_TYPE]:
+            for book_directory in get_items_in_dir(format_path(self.config[BOOK_DIRECTORY]), ["directories"]):
+                full_bookpath = format_path(self.config[BOOK_DIRECTORY] + book_directory)
+                if os.path.exists(full_bookpath + RESULTS_DIRECTORY):
+                    shutil.rmtree(full_bookpath + RESULTS_DIRECTORY)
+        if RUN_TYPE_SINGLE == self.config[RUN_TYPE]:
+            if os.path.exists(self.config[BOOK_DIRECTORY] + RESULTS_DIRECTORY):
+                shutil.rmtree(self.config[BOOK_DIRECTORY] + RESULTS_DIRECTORY)
+
+    def collate_results(self):
+
+        if RUN_TYPE_SINGLE == self.config[RUN_TYPE]:
+            self.__collate_results_on_book(self.config[BOOK_DIRECTORY] + RESULTS_DIRECTORY + os.sep)
+        elif RUN_TYPE_MULTI == self.config[RUN_TYPE]:
+            # 1. Created merged autocrop results for each book in the book directory
+            for book_directory in get_items_in_dir(format_path(self.config[BOOK_DIRECTORY]), ["directories"]):
+
+                full_bookpath = format_path(self.config[BOOK_DIRECTORY] + book_directory)
+
+                # Skip the QA log output directory if it exists in the book directory
+                if Path(self.config[OUTPUT_DIRECTORY]).name == Path(full_bookpath):
+                    continue
+                
+                # Write out a merged results file for this book in its results directory
+                try:
+                    self.__collate_results_on_book(full_bookpath + RESULTS_DIRECTORY + os.sep)
+                except:
+                    print("Collation of results for book {0} has failed.".format(book_directory))
+
+            # 2. Create one CSV file for all books in qa output directory
+            print("Merging all collated autocrop results")
+            self.__collate_all_book_results()
+
+    def __collate_all_book_results(self):
+
+        with open(self.config[OUTPUT_DIRECTORY] + "{0}_{1}.csv".format(MERGED_RESULTS_FILENAME_PREFIX, datetime.now().timestamp()), "w") as output_file:
+            
+            # 1. Read in collated results for each book and write them to the merged file
+            header_written = False
+            for book_directory in get_items_in_dir(format_path(self.config[BOOK_DIRECTORY]), ["directories"]):
+
+                # A. Get the latest collated csv file
+                results_directory = format_path(self.config[BOOK_DIRECTORY] + book_directory + os.sep + RESULTS_DIRECTORY)
+                csv_filepaths = []
+                for filepath in glob.glob(results_directory + "merged_*.csv"):
+                    csv_filepaths.append((filepath, os.path.getctime(filepath)))
+                if 0 == len(csv_filepaths):
+                    print("No collated csv file found for {0}.".format(book_directory))
+                    continue
+                sorted_csv_filepaths = sorted(csv_filepaths, key=lambda filepath: filepath[1], reverse=True)
+                latest_merged_filepath = sorted_csv_filepaths[0][0]
+
+                # B. Save the csv file contents to the merged file
+                with open(latest_merged_filepath, "r") as input_file:
+                    
+                    # Add the lines from this collated csv file (skipping the header if already written) to the all results csv file
+                    output_file.writelines(input_file.readlines()[1:] if header_written else input_file.readlines())
+                    header_written = True
+
+    def __collate_results_on_book(self, p_results_directory):
+
+        # 1. Get two most recent csv files for autocropping in the results directory (ignoring other collation csvs)
+        csv_filepaths = [(filepath, os.path.getctime(filepath)) \
+            for filepath in glob.glob(p_results_directory + "*.csv") if "merged_" not in filepath]
+        if len(csv_filepaths) < 2:
+            raise Exception("Less than two csv files in the results directory: {0}".format(p_results_directory))
+        sorted_csv_filepaths = sorted(csv_filepaths, key=lambda filepath: filepath[1], reverse=True)
+        results_filepath1, results_filepath2 = sorted_csv_filepaths[0][0], sorted_csv_filepaths[1][0]
+
+        # 2. Merge results file rows
+        with open(results_filepath1, "r") as results_file1:
+            
+            # A. Save rows from the first results file
+            csv_reader1 = csv.DictReader(results_file1)
+            results1_rows = []
+            for row in csv_reader1:
+                results1_rows.append({ key: row[key] for key in csv_reader1.fieldnames })
+
+            # B. Merge rows from the second results file with the first
+            with open(results_filepath2, "r") as results_file2:
+
+                # I. Grab second results file row
+                csv_reader2 = csv.DictReader(results_file2)
+                if csv_reader1.fieldnames != csv_reader2.fieldnames:
+                    raise Exception("Csv files being merged don't have same columns in the results directory: {0}".format(p_results_directory))
+                results2_rows = []
+                for row in csv_reader2:
+                    results2_rows.append({ key: row[key] for key in csv_reader2.fieldnames })
+
+                # II. Merge results file rows
+                merged_results = results1_rows.copy() + [row for row in results2_rows if "original" != row["autocrop_type"]] 
+
+        # 3. Write results into one csv file in the results directory
+        print("Writing merged results for results dir: {0}".format(p_results_directory))
+        with open(p_results_directory + "merged_results_{0}.csv".format(datetime.now().timestamp()), "w") as output_file:
+            csv_writer = csv.writer(output_file)
+
+            csv_writer.writerow([
+                "book_name",
+                "total_page_count",
+                "autocrop_type",
+                "image_name",
+                "image_width",
+                "image_height",
+                "min_pct_dimension_difference",
+                "image_area",
+                "area_diff_from_original",
+                "percent_area_diff_from_original",
+                "frobenius_norm_from_original"
+            ])
+
+            for row in merged_results:
+                csv_writer.writerow([
+                    row["book_name"],
+                    row["total_page_count"],
+                    row["autocrop_type"],
+                    row["image_name"],
+                    row["image_width"],
+                    row["image_height"],
+                    row["min_pct_dimension_difference"],
+                    row["image_area"],
+                    row["area_diff_from_original"],
+                    row["percent_area_diff_from_original"],
+                    row["frobenius_norm_from_original"]
+                ])
+
+    def run(self):
+
+        self.slurm_job_results = []
+        if RUN_TYPE_SINGLE == self.config[RUN_TYPE]:
+            self.slurm_job_results = self.__run_on_book(self.config[BOOK_DIRECTORY])
+        elif RUN_TYPE_MULTI == self.config[RUN_TYPE]:
+            self.slurm_job_results = self.__run_on_all_books()
+
+        print("Slurm Job Results")
+        for index in range(len(self.slurm_job_results)):
+            print("Result {0}: {1}".format(index, self.slurm_job_results[index]))
+
+    def __run_on_all_books(self):
+
+        return [ self.__run_on_book(format_path(self.config[BOOK_DIRECTORY] + book_name)) \
+            for book_name in get_items_in_dir(self.config[BOOK_DIRECTORY], ["directories"]) \
+            if Path(self.config[OUTPUT_DIRECTORY]).name != book_name ]
+
+    def __run_on_book(self, p_book_directory): 
+
+        # 1. Start a process to test autocropping methods on this book
+        book_name = Path(p_book_directory).name
+        print("Creating slurm job for QA of cropping " + book_name, flush=True)
+
+        # A. Build subprocess arguments for sbatch call
+        sbatch_args = {
+
+            "-c": "2",
+            "--mem-per-cpu": "1999mb",
+            "-t": "06:00:00",
+            "-o": "{0}slurm-{1}-{2}.out".format(self.config[OUTPUT_DIRECTORY], book_name, datetime.now().timestamp()),
+            "-p": "RM-shared"
+        }
+        subprocess_cmd = "sbatch"
+        for arg in sbatch_args:
+            subprocess_cmd += " {0} {1}".format(arg, sbatch_args[arg])
+        subprocess_cmd += " {0}{1}qa_autocrop.sh {2}".format(os.getcwd(), os.sep, p_book_directory)
+
+        print("subprocess.run({0}, capture_output=True, text=True, shell=True)".format(subprocess_cmd), flush=True)
+
+        return [subprocess.run(subprocess_cmd, capture_output=True, text=True, shell=True)]
 
 
 # Main script functions
