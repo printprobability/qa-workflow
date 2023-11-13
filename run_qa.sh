f81a9f366c46b295f01e9eeea271285c792a8681
jarmoza <jarmoza@gmail.com>
2023-11-13 10:12:56 -0500

    Updated sbatch directives

--- run_qa.sh
+++ run_qa.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
-#SBATCH -c 1
-#SBATCH --mem=1g
+#SBATCH -c 2
+#SBATCH --mem=1999mb
 #SBATCH -t 12:00:00
 #SBATCH -p RM-shared
-#SBATCH -o ./logs/qa_autocrop_slurm-%j.out
+#SBATCH -o ./logs/qa_slurm-%j.out
 
 if [ -z "$1" ] || [ -z "$2" ]
 then
     exit "run_qa.sh must be supplied with a qa module name and a config file."
 fi
 
-python3 -u qa.py $1 --config_file $2
+qa_module=$1
+config_filename=$2
+
+python3 -u qa.py $qa_module --config_file $config_filename
+
+# Collate results from the QA run
+# if [[ -v $QA_AUTOCROP_COLLATE_REQ ]]
+# then
+#   python3 -u qa.py $1 --collate --config_file $2 --run_uuid $QA_AUTOCROP_UUID
+# fi
